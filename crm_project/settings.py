dabb72ec8cdd73aa96f0366f3bf7b3daa349311d
Muhammad Ather <eshaal.zunaira@gmail.com>
2026-01-25 17:28:44 +0500

    Fix static files conflict

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -89,7 +89,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'static')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
